a9f204a3ca53e125ae7dc9b96845f91a2c0d6e0c
Matthew Peveler <matt.peveler@gmail.com>
2020-07-28 20:50:18 -0400

    more work
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
+echo "generating default.conf"
+
 cat > /etc/vpnc/default.conf <<EOF
 IPSec gateway ${VPNC_GATEWAY}
 IPSec ID ${VPNC_ID}
 IPSec secret ${VPNC_SECRET}
 Xauth username ${VPNC_USERNAME}
 Xauth password ${VPNC_PASSWORD}
-Domain ${VPNC_DOMAIN}
-DPD idle timeout (our side) 0
 EOF
 
-exec /usr/sbin/vpnc default --non-inter
+if [ ! -z ${VPNC_DOMAIN} ]; then
+    echo "Domain ${VPNC_DOMAIN}" >> /etc/vpnc/default.conf
+fi
+
+echo "DPD idle timeout (our side) 0" >> /etc/vpnc/default.conf
+
+echo "starting vpnc..."
+
+/usr/sbin/vpnc default --non-inter
+
+echo "running command..."
 
 bash -c "${1}"
